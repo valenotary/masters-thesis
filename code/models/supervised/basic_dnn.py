9c8f2deaba98d49c0196e69fd82d7738936107b5
Franz Anthony Varela <valenotary@gmail.com>
2022-02-16 22:43:56 -0800

    Update basic_dnn.py

--- code/models/supervised/basic_dnn.py
+++ code/models/supervised/basic_dnn.py
@@ -4,6 +4,8 @@ from torch.optim import Adam
 from torch import nn,tensor
 from pytorch_lightning import Trainer
 from pytorch_lightning.core.lightning import LightningModule
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
 
 class BasicDNNClassifier(LightningModule):
     def __init__(self, input_length: int, output_length: int):
@@ -27,13 +29,27 @@ class BasicDNNClassifier(LightningModule):
         logits = self(x)
         loss = F.nll_loss(logits, y)
         return loss
-    
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch 
+        logits = self(x) 
+        loss = F.nll_loss(logits, y)
+        self.loss("val_loss", loss)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        self.loss("test_loss", loss) 
+        return loss 
+
     def configure_optimizers(self):
         return Adam(self.parameters(), lr=1e-3)
 
 
 if __name__ == '__main__':
     net = BasicDNNClassifier(28*28, 10)
-    trainer = Trainer(gpus=1)
+    trainer = Trainer(gpus=1, callbacks=[EarlyStopping(monitor="val_loss", mode="min",patience=3)], logger=TensorBoardLogger("tb_logs", name="BasicDNNClassifer"))
     trainer.fit(net, datamodule=MNISTDataModule(num_workers=16))
 
